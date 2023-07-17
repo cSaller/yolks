4fc4121dfbc4ce9b8604a168a4b0d387b2b34d2e
Cleberson Saller <contato@csaller.com>
2023-07-17 02:58:59 +0000

    feat: added ruby files

--- ruby/entrypoint.sh
+++ ruby/entrypoint.sh
@@ -7,7 +7,9 @@ export INTERNAL_IP
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /home/container/tmp/pids/server.pid
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
